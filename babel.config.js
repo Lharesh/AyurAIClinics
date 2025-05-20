cd7b0cf7d133f2aef2c4be4f7dd58ebedf9a37cd
Lharesh <hareshlekkala@gmail.com>
2025-05-20 14:22:20 +0530

    Jest is working

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-transform-flow-strip-types',
+    [
+      '@babel/plugin-transform-typescript',
+      {
+        isTSX: true,
+        allExtensions: true,
+      },
+    ],
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  preset: 'react-native',
-  setupFiles: ['<rootDir>/jest.setup.js'],
-  transformIgnorePatterns: ['node_modules/(?!(react-native|@react-native|@react-navigation)/)'],
+  preset: 'ts-jest',
+  testEnvironment: 'node', // Use 'jsdom' if testing browser-based code
+  roots: ['<rootDir>/src'], // Adjust to your source folder
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // Use ts-jest to transform TypeScript files
+  },
+  testMatch: ['**/__tests__/**/*.(ts|tsx)', '**/?(*.)+(spec|test).(ts|tsx)'],
 };
